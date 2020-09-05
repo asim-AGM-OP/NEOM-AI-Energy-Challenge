6e2d59c33ad064fcd42f136c95049d721d55ae69
asim-AGM-OP <69813210+asim-AGM-OP@users.noreply.github.com>
2020-09-05 16:02:30 +0300

    Add files via upload

---
+++ ws_svm.m
@@ -0,0 +1,18 @@
+data = xlsread('C:\Users\hp\Desktop\MTLAB DATA FOR WT.xlsx'); % import data
+%% splitting data
+% training
+xt = data(1:1825, 1:4);
+yt = data(1:1825, 5);
+% testing
+xts = data(1826: end, 1:4);
+yts = data(1826: end, 5);
+
+%%svr
+MDL = fitrsvm(xt,yt,'Standardize',true,'KernelFunction','gaussian');
+p = predict(MDL,xts);
+rmse = sqrt(mean((p-yts).^2))
+
+plot(p,'r')
+hold on
+plot(yts,'b')
+
